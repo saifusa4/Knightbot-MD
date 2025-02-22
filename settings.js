8d8bd42826cb1a89fa4a0e9e53ce73e6830ad1e6
saifusa4 <mdsaifusa444@gmail.com>
2025-02-22 23:26:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "levi's bot",
+  botOwner: 'levi', // Your name
+  ownerNumber: '919959026648', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
